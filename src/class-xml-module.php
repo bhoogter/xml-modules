90e347f9d99fd9b81c1b28682101a1b71e8a1890
Ben <bhoogter@ford.com>
2019-11-29 20:36:21 -0500

    Init Repo

---
+++ src/class-xml-module.php
@@ -0,0 +1,40 @@
+<?php
+
+class xml_module extends xml_file
+	{
+	public $ID;
+	public $longdesc;
+	public $shortdesc;
+	public $version;
+	
+	public $Sources;
+	public $scan;
+	
+	public function clear()
+		{
+		parent::Clear();
+		
+		$this->ID  ="";
+		$this->longdesc = "";
+		$this->shortdesc = "";
+		$this->version = "";
+		return false;
+		}
+		
+	static function ModuleFile($m)	{return juniper_module_dir("$m/zmodule.xml");}
+	static function ModuleExists($m)	{return file_exists(self::ModuleFile($m));}
+    
+    function load_module($m)
+		{
+//print "<br/>load($m), MF=".self::ModuleFile($m);
+		if (!self::ModuleExists($m)) return false;
+		if (!$this->load(self::ModuleFile($m))) return false;
+		$this->ID = $m; // $this->fetch_part('/module/@name');
+//print "<br/>ModuleID: (f=".self::ModuleFile($m).") " . $this->ID;
+		$this->longdesc = $this->fetch_part('//module/module-specification/description/long');
+		$this->shortdesc = $this->fetch_part('//module/module-specification/description/short');
+		$this->version= $this->fetch_part('//module/module-specification/description/version');
+		return true;
+		}
+		
+	}
