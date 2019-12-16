af44be9706e8ea195dc106d0a0881d7c0f57e4a7
Ben <bhoogter@ford.com>
2019-12-16 12:14:08 -0500

    Fix tests

--- tests/class-xml-source-test.php
+++ tests/class-xml-source-test.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 // include_once('module_test.php');
-require_once(__DIR__ . "/../src/class-xml-source.php");
+require_once(__DIR__ . "/../src/class-source.php");
 
 class xml_source_test extends TestCase
 {
