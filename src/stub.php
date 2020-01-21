9ae16bf24af9318ff5312c463c515daca24a63f3
Ben <bhoogter@ford.com>
2020-01-21 13:02:44 -0500

    Include dm in dependencies

--- src/stub.php
+++ src/stub.php
@@ -1,6 +1,6 @@
 <?php
 // Place the dependency manager phar in the same directory ()
-define('DEPENDENCY_MANAGER_PHAR', __DIR__ . "/php-dependency-manager.phar");
+define('DEPENDENCY_MANAGER_PHAR', __DIR__ . "/phars/php-dependency-manager.phar");
 require_once("phar://" .    DEPENDENCY_MANAGER_PHAR . "/src/class-dependency-manager.php");
 dependency_manager("source", null, __DIR__ . "/phars/");
 
